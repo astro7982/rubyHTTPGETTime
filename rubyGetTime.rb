ca0347d31f1d0dfe3f206a789e5726dd943e84a0
John Campos <87489049+astro7982@users.noreply.github.com>
2021-07-29 19:47:34 -0400

    Removed code to install dependencies.

--- rubyGetTime.rb
+++ rubyGetTime.rb
@@ -1,39 +1,15 @@
 #!/usr/bin/ruby
+# Required dependencies
+require 'httpx'
 
-require 'rubygems'
-require 'rubygems/gem_runner'
-require 'rubygems/exceptions'
-
-puts 'Should install dependencies? Enter Yes or No.'
-puts ''
-puts 'If Yes, be sure to run as root (sudo).'
-installdep = gets.chomp
-
-def install(lib)
-  begin   
-  Gem::GemRunner.new.run ['install', lib]
-rescue Gem::SystemExitException => e
-end
-
-if %w[Yes yes y].include?(installdep)
-  # Install Dependencies
-  puts 'Installing Dependencies'
-  install 'httpx'
-else
-  # Checking Dependenices.
-  puts 'Checking Dependencies'
-  require 'httpx'
-end
-
-# base_url will get the current time. 
+# base_url will get the current time.
 base_url = 'http://worldtimeapi.org/api/timezone/America/New_York'
 response = HTTPX.get(base_url)
- 
 x = 0
 
 # until x >= 333 means it will get the base_url 333 times.
 until x >= 333
-  if response.status == 200 
+  if response.status == 200
     response2 = HTTPX.get(base_url)
     puts response2.body
     puts ''
