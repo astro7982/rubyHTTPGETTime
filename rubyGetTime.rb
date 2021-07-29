ced75fd312fd0eba64defb123a8ccc4f3641add5
John Campos <87489049+astro7982@users.noreply.github.com>
2021-07-29 19:58:54 -0400

    Changed the amount on loop.

--- rubyGetTime.rb
+++ rubyGetTime.rb
@@ -2,13 +2,13 @@
 # Required dependencies
 require 'httpx'
 
-# base_url will get the current time.
+# base_url will get the current time. Feel free to change the base_url to another.
 base_url = 'http://worldtimeapi.org/api/timezone/America/New_York'
 response = HTTPX.get(base_url)
 x = 0
 
-# until x >= 333 means it will get the base_url 333 times.
-until x >= 333
+# until x >= 50000 means it will get the base_url 50000 times.
+until x >= 500000
   if response.status == 200
     response2 = HTTPX.get(base_url)
     puts response2.body
